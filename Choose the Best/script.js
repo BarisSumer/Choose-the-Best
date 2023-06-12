69380988ef149f6ea0381885653f1f2e9086530c
BarisSumer <93003718+BarisSumer@users.noreply.github.com>
2023-06-13 01:54:14 +0300

    Choose-the-Best
    2022-2023 süper lig sezonunda futbolcu olan 64 kişiden her tur birini seçerek en iyi futbolcuyu seçme oyunudur.

---
+++ Choose the Best/script.js
@@ -0,0 +1,495 @@
+/*
+let data = [
+
+      {
+        "name": "Fernando Muslera",
+        "image": "https://resized-image.uwufufu.com/selection/16826812463434366/1024/crop_1682681244867.png"
+      },
+      {
+        "name": "Mauro İcardi",
+        "image": "https://resized-image.uwufufu.com/selection/16826811430911367/1024/crop_1682681139372.png"
+      },
+      {
+        "name": "Arda Güler",
+        "image": "https://resized-image.uwufufu.com/selection/16826840400647229/1024/crop_1682684037596.png"
+      },
+      {
+        "name": "Vincent Aboubakar",
+        "image": "https://resized-image.uwufufu.com/selection/16826839357019305/1024/crop_1682683934021.png"
+      },
+      {
+        "name": "Ferdi Kadıoğlu",
+        "image": "https://resized-image.uwufufu.com/selection/16826839839566912/1024/crop_1682683981987.png"
+      },
+      {
+        "name": "Enner Valencia",
+        "image": "https://resized-image.uwufufu.com/selection/1682681229483196/1024/crop_1682681226892.png"
+      },
+      {
+        "name": "Cenk Tosun",
+        "image": "https://resized-image.uwufufu.com/selection/1682683951311322/1024/crop_1682683948805.png"
+      },
+      {
+        "name": "Gedson Fernandes",
+        "image": "https://resized-image.uwufufu.com/selection/16826844230129099/1024/crop_1682684420354.png"
+      },
+      {
+        "name": "Sacha Boey",
+        "image": "https://resized-image.uwufufu.com/selection/16826815944592301/1024/crop_1682681592680.png"
+      },
+      {
+        "name": "Rachid Ghezzal",
+        "image": "https://resized-image.uwufufu.com/selection/16826846957352927/1024/crop_1682684693402.png"
+      },
+      {
+        "name": "Dries Mertens",
+        "image": "https://resized-image.uwufufu.com/selection/16826814516316936/1024/crop_1682681449827.png"
+      },
+      {
+        "name": "Victor Nelsson",
+        "image": "https://resized-image.uwufufu.com/selection/1682681739407583/1024/crop_1682681737279.png"
+      },
+      {
+        "name": "Mert Günok",
+        "image": "https://resized-image.uwufufu.com/selection/1682683891494828/1024/crop_1682683888820.png"
+      },
+      {
+        "name": "Nicolo Zaniolo",
+        "image": "https://resized-image.uwufufu.com/selection/16826813574793171/1024/crop_1682681354621.png"
+      },
+      {
+        "name": "Edin Visca",
+        "image": "https://resized-image.uwufufu.com/selection/16826852506792353/1024/crop_1682685248674.png"
+      },
+      {
+        "name": "Juan Mata",
+        "image": "https://resized-image.uwufufu.com/selection/16826839660347484/1024/crop_1682683963522.png"
+      },
+      {
+        "name": "Fabio Borini",
+        "image": "https://resized-image.uwufufu.com/selection/16826853131224036/1024/crop_1682685310703.png"
+      },
+      {
+        "name": "Nathan Redmond",
+        "image": "https://resized-image.uwufufu.com/selection/16826811264238569/1024/crop_1682681122416.png"
+      },
+      {
+        "name": "Lucas Torreira",
+        "image": "https://resized-image.uwufufu.com/selection/16826814896599362/1024/crop_1682681487430.png"
+      },
+      {
+        "name": "Kerem Aktürkoğlu",
+        "image": "https://resized-image.uwufufu.com/selection/16826817105775972/1024/crop_1682681708156.png"
+      },
+      {
+        "name": "Anastasios Bakasetas",
+        "image": "https://resized-image.uwufufu.com/selection/16826842614789740/1024/crop_1682684258921.png"
+      },
+      {
+        "name": "Mbaye Diagne",
+        "image": "https://resized-image.uwufufu.com/selection/16826864187783515/1024/crop_1682686416745.png"
+      },
+      {
+        "name": "Michy Batshuayi",
+        "image": "https://resized-image.uwufufu.com/selection/16826843553661292/1024/crop_1682684352511.png"
+      },
+      {
+        "name": "Miha Zajc",
+        "image": "https://resized-image.uwufufu.com/selection/16826852928424743/1024/crop_1682685290370.png"
+      },
+      {
+        "name": "Milot Rashica",
+        "image": "https://resized-image.uwufufu.com/selection/16826838608678963/1024/crop_1682683858853.png"
+      },
+      {
+        "name": "Miguel Crespo",
+        "image": "https://resized-image.uwufufu.com/selection/16826843954942275/1024/crop_1682684393113.png"
+      },
+      {
+        "name": "Dele Alli",
+        "image": "https://resized-image.uwufufu.com/selection/16826838766345833/1024/crop_1682683874388.png"
+      },
+      {
+        "name": "Attila Szalai",
+        "image": "https://resized-image.uwufufu.com/selection/16826840116027217/1024/crop_1682684009700.png"
+      },
+      {
+        "name": "Marek Hamsik",
+        "image": "https://resized-image.uwufufu.com/selection/16826862311095064/1024/crop_1682686228957.png"
+      },
+      {
+        "name": "Ali Sowe",
+        "image": "https://resized-image.uwufufu.com/selection/16826864428126333/1024/crop_1682686440509.png"
+      },  {
+        "name": "Yunus Akgün",
+        "image": "https://resized-image.uwufufu.com/selection/16826845215635890/1024/crop_1682684519938.png"
+      },
+      {
+        "name": "Valon Ethemi",
+        "image": "https://resized-image.uwufufu.com/selection/16826865188539027/1024/crop_1682686516516.png"
+      },
+      {
+        "name": "Sergio Oliveira",
+        "image": "https://resized-image.uwufufu.com/selection/16826815813336146/1024/crop_1682681577997.png"
+      },
+      {
+        "name": "Henry Onyekuru",
+        "image": "https://resized-image.uwufufu.com/selection/16826851249613442/1024/crop_1682685123106.png"
+      },
+      {
+        "name": "Abdulkerim Bardakçı",
+        "image": "https://resized-image.uwufufu.com/selection/16826838443119166/1024/crop_1682683841621.png"
+      },
+      {
+        "name": "Romain Saiss",
+        "image": "https://resized-image.uwufufu.com/selection/16826850779739564/1024/crop_1682685075678.png"
+      },
+      {
+        "name": "Valentin Rosier",
+        "image": "https://resized-image.uwufufu.com/selection/16826848792295983/1024/crop_1682684876954.png"
+      },
+      {
+        "name": "Uğurcan Çakır",
+        "image": "https://resized-image.uwufufu.com/selection/1682684123510692/1024/crop_1682684121995.png"
+      },
+      {
+        "name": "Younes Belhanda",
+        "image": "https://resized-image.uwufufu.com/selection/16826853329599707/1024/crop_1682685330432.png"
+      },
+      {
+        "name": "Maxi Gomez",
+        "image": "https://resized-image.uwufufu.com/selection/1682684501735921/1024/crop_1682684499443.png"
+      },
+      {
+        "name": "Haji Wright",
+        "image": "https://resized-image.uwufufu.com/selection/16826851090248593/1024/crop_1682685106843.png"
+      },
+      {
+        "name": "Osayi Samuel",
+        "image": "https://resized-image.uwufufu.com/selection/16826845486449204/1024/crop_1682684546414.png"
+      },
+      {
+        "name": "Abdülkadir Ömür",
+        "image": "https://resized-image.uwufufu.com/selection/16826844616811477/1024/crop_1682684459244.png"
+      },
+      {
+        "name": "Umut Nayir",
+        "image": "https://resized-image.uwufufu.com/selection/16826864642564031/1024/crop_1682686463198.png"
+      },
+      {
+        "name": "İrfancan Kahveci",
+        "image": "https://resized-image.uwufufu.com/selection/16826845665294480/1024/crop_1682684563987.png"
+      },
+      {
+        "name": "Talha Sanuç",
+        "image": "https://resized-image.uwufufu.com/selection/16826851825051465/1024/crop_1682685180398.png"
+      },
+      {
+        "name": "Yusuf Yazıcı",
+        "image": "https://resized-image.uwufufu.com/selection/16826842046479852/1024/crop_1682684202288.png"
+      },
+      {
+        "name": "Joshua King",
+        "image": "https://resized-image.uwufufu.com/selection/16826850200065924/1024/crop_1682685017956.png"
+      },
+      {
+        "name": "Adnan Januzaj",
+        "image": "https://resized-image.uwufufu.com/selection/16826845908939503/1024/crop_1682684588386.png"
+      },
+      {
+        "name": "Mahmoud Trezeguet",
+        "image": "https://resized-image.uwufufu.com/selection/16826843266908076/1024/crop_1682684324821.png"
+      },
+      {
+        "name": "Altay Bayındır",
+        "image": "https://resized-image.uwufufu.com/selection/16826841549956300/1024/crop_1682684152943.png"
+      },
+      {
+        "name": "Amir Hadziahmetovic",
+        "image": "https://resized-image.uwufufu.com/selection/16826846486606754/1024/crop_1682684645915.png"
+      },
+      {
+        "name": "Leo Dubois",
+        "image": "https://resized-image.uwufufu.com/selection/16826848537349537/1024/crop_1682684851834.png"
+      },
+      {
+        "name": "Diego Rossi",
+        "image": "https://resized-image.uwufufu.com/selection/16826841850587628/1024/crop_1682684182526.png"
+      },
+      {
+        "name": "Joao Pedro",
+        "image": "https://resized-image.uwufufu.com/selection/16826847146636259/1024/crop_1682684712321.png"
+      },
+      {
+        "name": "Fredrik Midtsjö",
+        "image": "https://resized-image.uwufufu.com/selection/16826849356895213/1024/crop_1682684933885.png"
+      },
+      {
+        "name": "Willian Arao",
+        "image": "https://resized-image.uwufufu.com/selection/16826848363711725/1024/crop_1682684834059.png"
+      },
+      {
+        "name": "Manolis Siopis",
+        "image": "https://resized-image.uwufufu.com/selection/16826852042618768/1024/crop_1682685201919.png"
+      },
+      {
+        "name": "Lincoln Henrique",
+        "image": "https://resized-image.uwufufu.com/selection/16826849808723845/1024/crop_1682684978897.png"
+      },
+      {
+        "name": "Dorukhan Toköz",
+        "image": "https://resized-image.uwufufu.com/selection/16826852324448142/1024/crop_1682685229713.png"
+      },
+      {
+        "name": "Enis Bardhi",
+        "image": "https://resized-image.uwufufu.com/selection/16826863397947564/1024/crop_1682686337288.png"
+      },
+      {
+        "name": "Ersin Destanoğlu",
+        "image": "https://resized-image.uwufufu.com/selection/16826846647196333/1024/crop_1682684662672.png"
+      },
+      {
+        "name": "Yusuf Demir",
+        "image": "https://resized-image.uwufufu.com/selection/16826850375482422/1024/crop_1682685034880.png"
+      },
+      {
+        "name": "Jean-Philippe Gbamin",
+        "image": "https://resized-image.uwufufu.com/selection/16826848068088297/1024/crop_1682684804833.png"
+      }
+    ]
+*/
+
+
+let data = [
+
+      {
+        "name": "Fernando Muslera",
+        "image": "https://resized-image.uwufufu.com/selection/16826812463434366/1024/crop_1682681244867.png"
+      },
+      {
+        "name": "Mauro İcardi",
+        "image": "https://resized-image.uwufufu.com/selection/16826811430911367/1024/crop_1682681139372.png"
+      },
+      {
+        "name": "Arda Güler",
+        "image": "https://resized-image.uwufufu.com/selection/16826840400647229/1024/crop_1682684037596.png"
+      },
+      {
+        "name": "Vincent Aboubakar",
+        "image": "https://resized-image.uwufufu.com/selection/16826839357019305/1024/crop_1682683934021.png"
+      },
+      {
+        "name": "Ferdi Kadıoğlu",
+        "image": "https://resized-image.uwufufu.com/selection/16826839839566912/1024/crop_1682683981987.png"
+      },
+      {
+        "name": "Enner Valencia",
+        "image": "https://resized-image.uwufufu.com/selection/1682681229483196/1024/crop_1682681226892.png"
+      },
+      {
+        "name": "Cenk Tosun",
+        "image": "https://resized-image.uwufufu.com/selection/1682683951311322/1024/crop_1682683948805.png"
+      },
+      {
+        "name": "Gedson Fernandes",
+        "image": "https://resized-image.uwufufu.com/selection/16826844230129099/1024/crop_1682684420354.png"
+      }
+    ]
+  
+
+
+let footballPlayersArray = Object.entries(data)
+
+
+const imageLeft = document.getElementById("imgLeft")
+const imageRight = document.getElementById("imgRight")
+const roundText = document.getElementById("roundText")
+const roundInfo = document.getElementById("roundInfo")
+const nameLeft = document.getElementById("nameLeft")
+const nameRight = document.getElementById("nameRight")
+
+let footballPlayersShuffledArray = shuffleArray(footballPlayersArray)
+
+let leftCounter = 0
+let rightCounter = 1
+let counter = 0
+
+
+let round = footballPlayersShuffledArray.length/2
+
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array
+}
+
+
+imageLeft.src = footballPlayersShuffledArray[leftCounter][1].image
+imageRight.src = footballPlayersShuffledArray[rightCounter][1].image
+printRoundNumber(counter,round)
+printRoundInfo(footballPlayersShuffledArray.length)
+printName()
+console.log(footballPlayersShuffledArray)
+
+function getNewImage(){
+  imageLeft.style.width = "400px"
+  imageRight.style.width = "400px"
+  imageRight.style.border = "3px solid #FF2E63"
+  imageLeft.style.border = "3px solid #08D9D6"
+  counter++
+  if(counter < round){
+    leftCounter ++
+    rightCounter ++
+    imageLeft.src = footballPlayersShuffledArray[leftCounter][1].image
+    imageRight.src = footballPlayersShuffledArray[rightCounter][1].image
+  }
+  else{
+    if(round == 1){
+      imageLeft.src = footballPlayersShuffledArray[0][1].image
+      imageRight.src = ""
+      imageLeft.removeEventListener("click", clickHandlerLeft)
+      imageRight.style.border = "none"
+      imageRight.remove()
+      imageLeft.style.width = "600px"
+      imageLeft.style.border = "3px solid #EAEAEA"
+      nameLeft.style.color = "#EAEAEA"
+    }
+    else{
+      let newShuffledArray = []
+      counter = 0
+      round = round / 2
+      leftCounter = 0
+      rightCounter = 1
+      newShuffledArray = shuffleArray(footballPlayersShuffledArray)
+      imageLeft.src = footballPlayersShuffledArray[leftCounter][1].image
+      imageRight.src = footballPlayersShuffledArray[rightCounter][1].image
+      printRoundInfo(footballPlayersShuffledArray.length)
+    }
+
+  }
+  
+}
+
+function deleteData(counter) {
+  footballPlayersShuffledArray.splice(counter,1)
+}
+
+function printRoundNumber(counter,round){
+  if(round == 1 && counter == 1){
+    roundText.textContent = "winner"
+  }
+  else if(round == 1 && counter != 1){
+    roundText.textContent = "final round"
+  }
+  else{
+    roundText.textContent = (counter + 1) + "/" + round 
+  }
+}
+
+function printRoundInfo(round){
+  if(round == 2){
+    roundInfo.textContent = ""
+  }
+  else if(round == 4){
+    roundInfo.textContent = "semi final"
+  }
+  else{
+    roundInfo.textContent = `round of last ${round}`
+  }
+}
+
+function printName(){
+  if(footballPlayersShuffledArray.length == 1){
+    nameLeft.textContent = footballPlayersShuffledArray[0][1].name
+    nameRight.textContent = ""
+  }
+  else{
+    nameLeft.textContent = footballPlayersShuffledArray[leftCounter][1].name
+    nameRight.textContent = footballPlayersShuffledArray[rightCounter][1].name
+  }
+ 
+}
+
+
+function setTimeotAllLeft(){
+  imageRight.style.display = ""
+  deleteData(rightCounter)
+  getNewImage()
+  printName()
+  console.log(counter)
+  console.log(footballPlayersShuffledArray)
+  printRoundNumber(counter,round)
+}
+
+function setTimeotAllRight(){
+  imageRight.style.display = ""
+  deleteData(leftCounter)
+  getNewImage()
+  printName()
+  console.log(counter)
+  console.log(footballPlayersShuffledArray)
+  printRoundNumber(counter,round)
+}
+
+
+
+
+
+let clickHandlerLeft = function(){
+  let src = imageLeft.src
+  imageLeft.src = src
+  imageRight.src = ""
+  imageLeft.style.width = "600px"
+  imageLeft.style.border = "3px solid #EAEAEA"
+  imageRight.style.border = "none"
+  imageRight.style.display = "none"
+  nameLeft.textContent = ""
+  nameRight.textContent = ""
+  console.log(src)
+  setTimeout(function(){
+    setTimeotAllLeft()
+  },1500)
+  
+}
+
+let clickHandlerRight = function(){
+  let src = imageRight.src
+  imageLeft.src = src
+  imageRight.src = ""
+  imageLeft.style.width = "600px"
+  imageLeft.style.border = "3px solid #EAEAEA"
+  imageRight.style.border = "none"
+  nameLeft.textContent = ""
+  imageRight.style.display = "none"
+  nameRight.textContent = ""
+  console.log(src)
+  setTimeout(function(){
+    setTimeotAllRight()
+  },1500)
+  
+}
+
+
+imageLeft.addEventListener("click",clickHandlerLeft)
+
+
+imageRight.addEventListener("click",clickHandlerRight)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
